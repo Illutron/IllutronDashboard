e673b62dc3f66f149ee0db8bc4dc49f2c67d4926
Johan <jbl@1calendar.com>
2011-10-09 20:00:08 +0200

    Removed local settings from git.

--- settings_local.py
+++
@@ -1,9 +0,0 @@
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'b-6!khjsadlgfiiawer3bv%4kcv-^h1tag6e_!2_iw'
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.db',                      # Or path to database file if using sqlite3.
-    }
-}
